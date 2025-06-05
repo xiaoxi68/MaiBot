85d543d55584b90683eb007ee8242e487d738710
春河晴 <bbleae@outlook.com>
2025-06-05 15:49:05 +0900

    ruff

--- src/common/tcp_connector.py
+++ src/common/tcp_connector.py
@@ -1,10 +1,10 @@
 import ssl
 import certifi
 import aiohttp
-import asyncio
 
 ssl_context = ssl.create_default_context(cafile=certifi.where())
 connector = None
+
 
 async def get_tcp_connector():
     global connector
