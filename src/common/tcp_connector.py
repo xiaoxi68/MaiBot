fc374903a2efb422df17d08b7f797ee6dbc70998
晴猫 <bbleae@outlook.com>
2025-06-05 17:03:13 +0900

    fix session closed

--- src/common/tcp_connector.py
+++ src/common/tcp_connector.py
@@ -3,11 +3,7 @@ import certifi
 import aiohttp
 
 ssl_context = ssl.create_default_context(cafile=certifi.where())
-connector = None
 
 
 async def get_tcp_connector():
-    global connector
-    if connector is None:
-        connector = aiohttp.TCPConnector(ssl=ssl_context)
-    return connector
+    return aiohttp.TCPConnector(ssl=ssl_context)
