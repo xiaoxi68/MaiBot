3e53c8a326b37f3b561321b54122acd12dd577d8
SengokuCola <1026294844@qq.com>
2025-06-12 23:49:08 +0800

    Update logger.py

--- src/common/logger.py
+++ src/common/logger.py
@@ -150,8 +150,11 @@ MODULE_COLORS = {
     "plugins": "\033[31m",  # 红色
     "plugin_api": "\033[33m",  # 黄色
     "remote": "\033[38;5;93m",  # 紫蓝色
-    "planner": "\033[31m",
+    "planner": "\033[36m",
     "memory": "\033[34m",
+    "hfc": "\033[96m",
+    "base_action": "\033[96m",
+    "action_manager": "\033[34m]"
 }
 
 RESET_COLOR = "\033[0m"
